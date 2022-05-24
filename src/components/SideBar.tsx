6ea894fa3702c67b1eb67fe988ac4f68feb099eb
Bruno Gomes <brunogxy10@gmail.com>
2022-05-24 14:13:05 -0300

    desafio ignite 02 resolvido

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from '../components/Button';
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: Function;
+}
+
+export function SideBar({
+  genres,
+  setSelectedGenreId,
+  selectedGenreId,
+}: SideBarProps) {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
